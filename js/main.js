d253f06ae3d84ced3cb71b66a7843e40ab8769b1
faster2020 <sergey.ganus@mail.ru>
2020-04-14 17:13:14 +0300

    Adaptive for modal window of shopping cart

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
-const cartButton = document.querySelector("#cart-button");
+const cartButton = document.querySelector("#button-cart");
+const cancelButton = document.querySelector("#button-cancel");
 const modal = document.querySelector(".modal");
 const modalClose = document.querySelector(".modal__close");
 
 cartButton.addEventListener("click", ToogleModal);
 
 modalClose.addEventListener("click", ToogleModal);
+cancelButton.addEventListener("click", ToogleModal);
+modal.addEventListener("click", CloseModal);
 
 function ToogleModal () {
-  modal.classList.toggle("is-active");
+    modal.classList.toggle("is-active");
+    event.stopPropagation();
+};
+
+function CloseModal () {
+  if (event.target.className == modal.className) {
+    modal.classList.remove("is-active");
+  }
 };
 
 new WOW().init();
